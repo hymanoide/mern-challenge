e22fa97385935af8b3870e7e9d37d77bf8968d4b
cbenitez <cbenitez@rodi.es>
2023-07-21 17:13:51 +0200

    chore: adapt db adapter to connection string env variable

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
+const {mongoDBConnectionString} = require('../config/config');
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/mern-stack', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(mongoDBConnectionString, {useNewUrlParser: true, useUnifiedTopology: true})
     .catch(e => {
         console.error('Connection error', e.message)
     });
 
-const db = mongoose.connection;
-
-module.exports = db;
+module.exports = mongoose.connection;
